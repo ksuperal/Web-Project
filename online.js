c258ed7a719cc60035d9cf12875c440a2fd1d4b6
ksuperal <singhsuwan3.1@gmail.com>
2023-10-14 18:00:39 +0700

    css modify on online learning

--- online.js
+++ online.js
@@ -1,3 +1,17 @@
-addvideo(){
-    
-}
+// Get the video file input element
+const videoInput = document.getElementById('video-input');
+
+// Listen for changes to the input element
+videoInput.addEventListener('change', () => {
+    // Get the selected file
+    const file = videoInput.files[0];
+
+    // Create a URL for the selected file
+    const fileURL = URL.createObjectURL(file);
+
+    // Get the inner-video div element
+    const innerVideo = document.getElementById('inner-video');
+
+    // Set the innerHTML of the inner-video div to an HTML video element with the selected file as the source
+    innerVideo.innerHTML = `<video src="${fileURL}" controls></video>`;
+});
